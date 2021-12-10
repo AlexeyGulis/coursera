5e2059577499cc6696127b0fabeb90b2b57ec7f3
AlexeyGulis <kiri.dh@yandex.ru>
2021-12-10 13:46:52 +0300

    bruteforce

--- src/kdTree/PointSET.java
+++ src/kdTree/PointSET.java
@@ -24,40 +24,40 @@ public class PointSET {
     }
 
     public void insert(Point2D p) {
-        if(p == null) throw new IllegalArgumentException();
+        if (p == null) throw new IllegalArgumentException();
         point2DSet.add(p);
     }
 
     public boolean contains(Point2D p) {
-        if(p == null) throw new IllegalArgumentException();
+        if (p == null) throw new IllegalArgumentException();
         return point2DSet.contains(p);
     }
 
     public void draw() {
         for (Point2D p : point2DSet
-             ) {
+        ) {
             p.draw();
         }
     }
 
     public Iterable<Point2D> range(RectHV rect) {
-        if(rect == null) throw new IllegalArgumentException();
+        if (rect == null) throw new IllegalArgumentException();
         TreeSet<Point2D> rangeSet = new TreeSet<>();
         for (Point2D p : point2DSet
-             ) {
-            if(rect.contains(p)) rangeSet.add(p);
+        ) {
+            if (rect.contains(p)) rangeSet.add(p);
         }
         return rangeSet;
     }
 
     public Point2D nearest(Point2D p) {
-        if(p == null) throw new IllegalArgumentException();
+        if (p == null) throw new IllegalArgumentException();
         Point2D nearest = null;
         for (Point2D c : point2DSet
-             ) {
-            if(nearest == null){
+        ) {
+            if (nearest == null) {
                 nearest = c;
-            }else if(c.distanceTo(p) < nearest.distanceTo(p)){
+            } else if (c.distanceTo(p) < nearest.distanceSquaredTo(p)) {
                 nearest = c;
             }
         }
@@ -66,13 +66,13 @@ public class PointSET {
 
     public static void main(String[] args) {
         PointSET set = new PointSET();
-        set.insert(new Point2D(0.7,0.2));
-        set.insert(new Point2D(0.5,0.4));
-        set.insert(new Point2D(0.2,0.3));
-        set.insert(new Point2D(0.4,0.7));
-        set.insert(new Point2D(0.9,0.6));
-        for (Point2D d : set.range(new RectHV(0.3,0.3,1.0,1.0))
-             ) {
+        set.insert(new Point2D(0.7, 0.2));
+        set.insert(new Point2D(0.5, 0.4));
+        set.insert(new Point2D(0.2, 0.3));
+        set.insert(new Point2D(0.4, 0.7));
+        set.insert(new Point2D(0.9, 0.6));
+        for (Point2D d : set.range(new RectHV(0.3, 0.3, 1.0, 1.0))
+        ) {
             StdOut.println(d);
         }
     }
