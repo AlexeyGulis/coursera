9e450002a36b6ed31a801eef4756d86b361eac9d
AlexeyGulis <kiri.dh@yandex.ru>
2021-12-10 09:20:59 +0300

    K-d Tree contains

--- src/kdTree/KdTree.java
+++ src/kdTree/KdTree.java
@@ -2,6 +2,7 @@ package kdTree;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.TreeSet;
 
@@ -44,28 +45,17 @@ public class KdTree {
         if (search == null) return new Node(null, null, 1, newPoint);
         int cmp;
         int cmpAlt;
-        Double key1;
-        Double key2;
         if (rotate) {
-            key1 = search.key.x();
-            key2 = newPoint.x();
-            cmp = key1.compareTo(key2);
-            key1 = search.key.y();
-            key2 = newPoint.y();
-            cmpAlt = key1.compareTo(key2);
+            cmp = comparePoint(search.key.x(), newPoint.x());
+            cmpAlt = comparePoint(search.key.y(), newPoint.y());
         } else {
-            key1 = search.key.y();
-            key2 = newPoint.y();
-            cmp = key1.compareTo(key2);
-            key1 = search.key.x();
-            key2 = newPoint.x();
-            cmpAlt = key1.compareTo(key2);
+            cmp = comparePoint(search.key.y(), newPoint.y());
+            cmpAlt = comparePoint(search.key.x(), newPoint.x());
         }
-        if (cmp > 0){
+        if (cmp > 0) {
             search.left = insert(search.left, newPoint, !rotate);
-        }
-        else if (cmp <= 0) {
-            if(cmpAlt != 0){
+        } else if (cmp <= 0) {
+            if (cmpAlt != 0) {
                 search.right = insert(search.right, newPoint, !rotate);
             }
         }
@@ -74,15 +64,46 @@ public class KdTree {
         return search;
     }
 
-    private int size(Node t){
-        if(t == null) return 0;
+    private int comparePoint(Double p1, Double p2) {
+        return p1.compareTo(p2);
+    }
+
+    private int size(Node t) {
+        if (t == null) return 0;
         else return t.size;
     }
 
     public boolean contains(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
-        return false;
+        boolean result = false;
+        boolean rotate = true;
+        Node t = topNode;
+        int cmp;
+        int cmpAlt;
+        while (t != null) {
+            if (t.key.equals(p)) {
+                return true;
+            }
+            if (rotate) {
+                cmp = comparePoint(t.key.x(), p.x());
+                cmpAlt = comparePoint(t.key.y(), p.y());
+            } else {
+                cmp = comparePoint(t.key.x(), p.x());
+                cmpAlt = comparePoint(t.key.y(), p.y());
+            }
+            if (cmp > 0) {
+                t = t.left;
+                rotate = !rotate;
+            } else if (cmp <= 0) {
+                if (cmpAlt != 0) {
+                    t = t.right;
+                    rotate = !rotate;
+                }
+            }
+        }
+        return result;
     }
+
 
     public void draw() {
 
@@ -102,11 +123,12 @@ public class KdTree {
 
     public static void main(String[] args) {
         KdTree kdTree = new KdTree();
-        kdTree.insert(new Point2D(0.7,0.2));
-        kdTree.insert(new Point2D(0.7,0.3));
-        kdTree.insert(new Point2D(0.5,0.4));
-        kdTree.insert(new Point2D(0.2,0.3));
-        kdTree.insert(new Point2D(0.4,0.7));
-        kdTree.insert(new Point2D(0.9,0.6));
+        kdTree.insert(new Point2D(0.7, 0.2));
+        kdTree.insert(new Point2D(0.7, 0.3));
+        kdTree.insert(new Point2D(0.5, 0.4));
+        kdTree.insert(new Point2D(0.2, 0.3));
+        kdTree.insert(new Point2D(0.4, 0.7));
+        kdTree.insert(new Point2D(0.9, 0.6));
+        StdOut.println(kdTree.contains(new Point2D(0.5,0.2)));
     }
 }
