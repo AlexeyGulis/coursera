8860f289a7debd5ae172bde9300114b84472d6de
AlexeyGulis <kiri.dh@yandex.ru>
2022-01-07 13:40:06 +0300

    test

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,12 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import sliderPuzzle.Board;
 
 public class Test {
     public static void main(String[] args) {
-        double a = Double.NaN;
-        double b = Double.NaN;
-        System.out.println(a == b);
-        System.out.println(Double.valueOf(a).equals(Double.valueOf(b)));
+        In in = new In(args[0]);
+        String[] nouns = in.readAllStrings();
+        System.out.println();
     }
 }
