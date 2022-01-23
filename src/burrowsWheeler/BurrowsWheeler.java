8c5c461b9026250ec1e9f353a45dc1d45d644b94
AlexeyGulis <kiri.dh@yandex.ru>
2022-01-23 16:51:35 +0300

    BurrowsWheeler start testing

--- src/burrowsWheeler/BurrowsWheeler.java
+++ src/burrowsWheeler/BurrowsWheeler.java
@@ -3,32 +3,69 @@ package burrowsWheeler;
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
+import java.util.Arrays;
+
 public class BurrowsWheeler {
-    public static void transform(){
-        if(BinaryStdIn.isEmpty()) return;
+    private static char[] t;
+    public static void transform() {
+        if (BinaryStdIn.isEmpty()) return;
         String str = BinaryStdIn.readString();
         CircularSuffixArray circularSuffixArray = new CircularSuffixArray(str);
         StringBuilder stringBuilder = new StringBuilder();
         int number = 0;
         for (int i = 0; i < circularSuffixArray.length(); i++) {
-            if(circularSuffixArray.index(i) == 0){
+            if (circularSuffixArray.index(i) == 0) {
                 number = i;
                 stringBuilder.append(str.charAt(str.length() - 1));
-            }else{
+            } else {
                 stringBuilder.append(str.charAt(circularSuffixArray.index(i) - 1));
             }
         }
         BinaryStdOut.write(number);
-        BinaryStdOut.write(stringBuilder.toString());
+        BinaryStdOut.write(stringBuilder.toString(), 8);
     }
-    public static void inverseTransform(){
 
+    public static void inverseTransform() {
+        int start = BinaryStdIn.readInt();
+        if (BinaryStdIn.isEmpty()) return;
+        t = BinaryStdIn.readString().toCharArray();
+        Custom[] customs = new Custom[t.length];
+        for (int i = 0; i < t.length; i++) {
+            customs[i] = new Custom(i, t[i]);
+        }
+        Arrays.sort(customs);
+        int j = start;
+        for (int i = 0; i < t.length; i++) {
+            if(i == 0) j = start;
+            BinaryStdOut.write(customs[j].getB());
+            j = customs[j].getI();
+        }
+    }
+
+    private static class Custom implements Comparable<Custom> {
+        private int i;
+        private char b;
+        private Custom(int i, char b) {
+            this.i = i;
+            this.b = b;
+        }
+
+        private int getI() {
+            return i;
+        }
+        private char getB(){
+            return b;
+        }
+        @Override
+        public int compareTo(Custom o) {
+            return t[this.getI()] > t[o.getI()] ? 1 : t[this.getI()] < t[o.getI()] ? -1 : 0;
+        }
     }
 
     public static void main(String[] args) {
-        if(args[0].equals("-")){
+        if (args[0].equals("-")) {
             transform();
-        }else if(args[0].equals("-")){
+        } else if (args[0].equals("+")) {
             inverseTransform();
         }
     }
--- src/burrowsWheeler/CircularSuffixArray.java
+++ src/burrowsWheeler/CircularSuffixArray.java
@@ -35,7 +35,7 @@ public class CircularSuffixArray {
             this.i = i;
         }
 
-        public int getI() {
+        private int getI() {
             return i;
         }
 
