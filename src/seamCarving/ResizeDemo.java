67248257be0c8217ec4521d1909a780d3fa93886
AlexeyGulis <kiri.dh@yandex.ru>
2022-01-31 10:56:01 +0300

    SeamCarving test1

--- src/seamCarving/ResizeDemo.java
+++ src/seamCarving/ResizeDemo.java
@@ -14,6 +14,8 @@ package seamCarving;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
 
+import java.io.File;
+
 public class ResizeDemo {
     public static void main(String[] args) {
         if (args.length != 4) {
@@ -23,32 +25,38 @@ public class ResizeDemo {
         String folder = args[0];
         String safeFolder = args[3];
         Stopwatch sw = new Stopwatch();
-        for (int i = 0; i < 101; i++) {
+        for (int j = 0; j < 101; j++) {
             StringBuilder str = new StringBuilder();
-            str.append("Img_0").append(String.format("%03d", i)).append(".tif");
+            str.append("Img_0").append(String.format("%03d", j)).append(".tif");
             Picture inputImg = new Picture(folder + str);
-            int removeColumns = Integer.parseInt(args[1]);
-            int removeRows = Integer.parseInt(args[2]);
-
-            //StdOut.printf("image is %d columns by %d rows\n", inputImg.width(), inputImg.height());
+            int removeColumns = (int) (inputImg.height() * .9);
+            int removeRows = (int) (inputImg.width() * .9);
             SeamCarverOpt sc = new SeamCarverOpt(inputImg);
-
-            for (int j = 0; j < removeRows; j++) {
-                int[] horizontalSeam = sc.findHorizontalSeam();
-                sc.removeHorizontalSeam(horizontalSeam);
+            int count = 0;
+            int p = 0;
+            int k = 0;
+            while (count < removeColumns + removeRows) {
+                if (p < removeRows) {
+                    int[] horizontalSeam = sc.findHorizontalSeam();
+                    sc.removeHorizontalSeam(horizontalSeam);
+                    p++;
+                }
+                if (k < removeColumns) {
+                    int[] verticalSeam = sc.findVerticalSeam();
+                    sc.removeVerticalSeam(verticalSeam);
+                    k++;
+                }
+                if ((((double) (inputImg.height() - p - 1) / (double) inputImg.height()) * 100) % 10 == 0) {
+                    File dirSave = new File(safeFolder + (inputImg.height() - p - 1));
+                    if (!dirSave.exists()) {
+                        dirSave.mkdir();
+                    }
+                    Picture outputImg = sc.picture();
+                    outputImg.save(dirSave + "\\" + str);
+                }
+                count++;
             }
-
-            for (int j = 0; j < removeColumns; j++) {
-                int[] verticalSeam = sc.findVerticalSeam();
-                sc.removeVerticalSeam(verticalSeam);
-            }
-            Picture outputImg = sc.picture();
-
-            outputImg.save(safeFolder + str);
-
             //StdOut.printf("new image size is %d columns by %d rows\n", sc.width(), sc.height());
-
-
         }
         StdOut.println("Resizing time: " + sw.elapsedTime() + " seconds.");
     }
